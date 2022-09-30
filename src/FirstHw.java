095efc662cc4b52f160a54a2afafb46b9bd0af3d
yasinmurat <muratyeni671@gmail.com>
2022-10-01 00:32:31 +0300

    hw1bıttı

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,5 +1,6 @@
 public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
+        System.out.println("merhaba dünya");
     }
 }
